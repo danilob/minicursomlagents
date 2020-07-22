b217e37122fde398d6d8558c3a8bbae0de7bab17
vitorAzevedo009 <vitor.azevedo009@gmail.com>
2020-07-22 09:29:34 -0300

    Traduzindo os comentarios no script do Teste01 e modificando a posição dos atributos para que fiquem juntos no começo da classe

--- Assets/RollerAgent.cs
+++ Assets/RollerAgent.cs
@@ -7,64 +7,67 @@ using Unity.MLAgents.Sensors;
 public class RollerAgent : Agent
 {
 	Rigidbody rBody;
+	public Transform target;
+	public float speed = 10;
+
 	void Start () {
 		rBody = GetComponent<Rigidbody>();
 	}
 
-	public Transform Target;
 	public override void OnEpisodeBegin()
 	{
 		if (this.transform.localPosition.y < 0)
 		{
-		    // If the Agent fell, zero its momentum
+		    // Se o agente cair da plataforma, o seu momento é zero.
 		    this.rBody.angularVelocity = Vector3.zero;
 		    this.rBody.velocity = Vector3.zero;
 		    this.transform.localPosition = new Vector3( 0, 0.5f, 0);
 		}
 
-		// Move the target to a new spot
-		Target.localPosition = new Vector3(Random.value * 8 - 4,
-				                   0.5f,
-				                   Random.value * 8 - 4);
+		// Move o alvo para um novo ponto
+		target.localPosition = new Vector3(Random.value * 8 - 4, 0.5f, Random.value * 8 - 4);
 	}
 
 	public override void CollectObservations(VectorSensor sensor)
 	{
-	    // Target and Agent positions
-	    sensor.AddObservation(Target.localPosition);
+	    // Adicionando as posições do agente e do alvo para serem observadas.
+	    sensor.AddObservation(target.localPosition);
 	    sensor.AddObservation(this.transform.localPosition);
 
-	    // Agent velocity
-	    //sensor.AddObservation(rBody.velocity);
+	    // Adicionando a velocidade do agente para ser observada.
 		sensor.AddObservation(rBody.velocity.x);
 	    sensor.AddObservation(rBody.velocity.z);
 	}
 
-	public float speed = 10;
 	public override void OnActionReceived(float[] vectorAction)
 	{
-	    // Actions, size = 2
+	    // Vetor de ações com tamanho dois.
 	    Vector3 controlSignal = Vector3.zero;
 	    controlSignal.x = vectorAction[0];
 	    controlSignal.z = vectorAction[1];
-		//controlSignal.z = vectorAction[2];
+
+		// Adicionando uma força ao agente de acordo com os valores retornados do vetor ação da rede neural.
 	    rBody.AddForce(controlSignal * speed);
 
-	    // Rewards
-	    float distanceToTarget = Vector3.Distance(this.transform.localPosition, Target.localPosition);
+	    // Distância entre o agente e o alvo.
+	    float distanceTotarget = Vector3.Distance(this.transform.localPosition, target.localPosition);
 
-	    // Reached target
-	    if (distanceToTarget < 1.42f)
-	    {
-		SetReward(1.0f);
-		EndEpisode();
-	    }
+		//Recompensas
 
-	    // Fell off platform
-	    if (this.transform.localPosition.y < 0)
-	    {
-		EndEpisode();
-	    }
+	    	// Se atingir o alvo, o agente é recompensado positivamente.
+	    	if (distanceTotarget < 1.42f)
+	    	{
+			SetReward(1.0f);
+			EndEpisode();
+	    	}
+
+	   		// Se o agente cair, o episódio se encerra, sem o agente ser recompensado.
+	    	if (this.transform.localPosition.y < 0)
+	    	{
+			EndEpisode();
+	    	}
+		
+		// Fim das Recompensas
 	}
 
 }
